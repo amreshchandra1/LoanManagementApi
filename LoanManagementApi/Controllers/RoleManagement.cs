08faab0d674f5b772e2b13bfb7bdf8a237ea25e1
amresh <amresh@AMRESH>
2026-07-30 10:38:57 +0530

    Added validation for Roles

--- LoanManagementApi/Controllers/RoleManagement.cs
+++ LoanManagementApi/Controllers/RoleManagement.cs
@@ -1,4 +1,6 @@
-﻿using LoanManagementApi.Repository;
+﻿using FluentValidation;
+using LoanManagementApi.Model;
+using LoanManagementApi.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,15 +13,27 @@ namespace LoanManagementApi.Controllers
     {
         private readonly ILogger<RoleManagement> _logger;
         private readonly IRoleManagement _roleManagementRepository;
-        public RoleManagement(ILogger<RoleManagement> logger,IRoleManagement roleManagementRepository)
+        private readonly IValidator<Roles> _validator;
+        public RoleManagement(ILogger<RoleManagement> logger,IRoleManagement roleManagementRepository, IValidator<Roles> validator)
         {
             _logger = logger;
             _roleManagementRepository = roleManagementRepository;
+            _validator = validator;
 
         }
         [HttpGet("AddRole")]
-        public IActionResult AddRole(string roleName)
+        public async Task< IActionResult> AddRole(string roleName)
         {
+            var validationResult = await _validator.ValidateAsync(new Roles() {RoleName=roleName });
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return BadRequest(ModelState);
+            }
             int result=_roleManagementRepository.AddRole(roleName);
             if (result > 0)
             {
---
+++ LoanManagementApi/Validation/RolesValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using LoanManagementApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanManagementApi.Validation
+{
+    public class RolesValidator:AbstractValidator<Roles>
+    {
+        private readonly EFContext _context;
+        public RolesValidator(EFContext context)
+        {
+            _context = context;
+            RuleFor(role => role.RoleName)
+             .MustAsync(async (roleInstance, roleName, cancellationToken) =>
+             {
+                 var exists = await _context.Roles.AnyAsync(r =>
+                     r.RoleName.ToLower() == roleName.ToLower() &&
+                     r.Id != roleInstance.Id,
+                     cancellationToken);
+                 // Return true if it does NOT exist (meaning it is valid)
+                 return !exists;
+             })
+             .WithMessage("This role name already exists.");
+        }
+    }
+}
